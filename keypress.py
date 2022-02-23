58440aebabf229c654716a57f69af2c05282dde0
magavuz77 <tatul.danielyan.s@gmail.com>
2022-02-23 19:22:10 +0400

    added comments for better understanding

--- keypress.py
+++ keypress.py
@@ -8,11 +8,11 @@ def get_key(keyName):
     ans = False
     for event in pygame.event.get():
         pass
-    keyInput = pygame.key.get_pressed()
-    myKey = getattr(pygame, 'K_{}'.format(keyName))
+    keyInput = pygame.key.get_pressed() # returns an array of boolean values 
+    myKey = getattr(pygame, 'K_{}'.format(keyName)) # get the key index in keyInput array
     #print('K_{}'.format(keyName))
-
-    if keyInput[myKey]:
+    print(myKey)
+    if keyInput[myKey]:# checking index of key in array (True or False)
         ans = True
 
     pygame.display.update()
@@ -25,7 +25,6 @@ def main():
     if get_key('d'):
         print('Right key pressed.')
         
-
 if __name__ == '__main__':
     init()
     while True:
